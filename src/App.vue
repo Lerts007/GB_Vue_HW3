afa31296600ba3a0e4ea74f4a7d678698fbda8cc
Lerts_W <oddff@mail.ru>
2024-01-29 12:52:42 +0300

    HW_3

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ProductDetails
+    v-for="(product, index) in products"
+    :key="index"
+    :product="product"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductDetails from "./components/ProductDetails.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { ProductDetails },
+  data() {
+    return {
+      products: [
+        {
+          name: "Phone",
+          price: 99.99,
+          available: false,
+        },
+        {
+          name: "Fridge",
+          price: 59.99,
+          available: true,
+        },
+        {
+          name: "Video camera",
+          price: 199.0,
+          available: true,
+        },
+        {
+          name: "Hoover",
+          price: 38.49,
+          available: true,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss">
 #app {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="productDetails">
+    <img src="..//assets/logo.png" alt="" />
+    <p>{{ product.name }}</p>
+    <p>{{ price }}</p>
+    <p v-if="product.available">Available</p>
+    <p v-else="product.available">Out of stock</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: Object,
+  },
+  computed: {
+    price() {
+      return "$" + this.product.price;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.productDetails {
+  width: 300px;
+  height: 400px;
+  border: 2px solid #2c3e50;
+  border-radius: 5px;
+}
+</style>
